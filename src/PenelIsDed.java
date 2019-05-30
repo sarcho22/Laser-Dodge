620f2ef05b53a165df0c104fba397eb38b4ac20d
penelopeom <penelopeom@aol.com>
2019-05-30 13:01:25 -0400

    yeet yeet i'm not a shrimp

--- src/PenelIsDed.java
+++ src/PenelIsDed.java
@@ -1,2 +1,21 @@
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class PenelIsDed {
+    public void start (Stage primaryStage) {
+        Pane pane = new Pane();
+        Image marioImage = new Image("mario.png");
+        ImageView mario = new ImageView(marioImage);
+        mario.setFitHeight(58);
+        mario.setFitWidth(72);
+        pane.getChildren().add(mario);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mario YEET");
+        primaryStage.show();
+    }
 }
