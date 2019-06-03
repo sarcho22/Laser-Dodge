4de0b0095342bf25485a1bdbef43f2ec279532b9
alevol22 <alevol22@bergen.org>
2019-06-03 15:17:40 -0400

    my page

--- src/SashaIsGone.java
+++ src/SashaIsGone.java
@@ -9,6 +9,21 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class SashaIsGone extends Application {
     public void start (Stage primaryStage) {
@@ -109,3 +124,130 @@ class drawLine1 extends Pane{
     }
 
 }
+
+
+
+/*int score = 0;
+        boolean p1alive = true;
+
+        while(p1alive) {
+//            lineline laser = new lineline();
+            for(int i = 0; i < score; i++) {
+
+
+                Line line = new Line();
+                line.setFill(Color.WHITE);
+                line.setStroke(Color.RED);
+                pane.getChildren().add(line);
+                Bounds lineBounds = line.getBoundsInParent();
+                int direction = (int)(Math.random() * 2);
+                if(direction == 0){
+                    line.setStartX(0);
+                    line.setStartY(Math.random() * 500);
+                    line.setEndX(500);
+                    line.setEndY(Math.random() * 500);
+                }
+                else{
+                    line.setStartX(Math.random() * 500);
+                    line.setStartY(0);
+                    line.setEndX(Math.random() * 500);
+                    line.setEndY(500);
+                }
+                Timeline timer = new Timeline(
+                        new KeyFrame(Duration.ZERO),
+                        new KeyFrame(Duration.seconds(0.5))
+                );
+//            timer.setAutoReverse(true);
+                timer.setCycleCount(5);
+                if (isIntersect(p1Bounds, lineBounds)) {
+                    p1alive = false;
+                    break;
+                }
+
+
+            }
+            if(p1alive) {
+                score++;
+                pane.getChildren().clear();
+            }
+            else {
+                TextField ded = new TextField("YOU GO AWAY U DIED");
+                break;
+            }
+//
+
+
+        }
+
+
+
+        pane.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.RIGHT){
+                if(p1.getX() + p1.getWidth() >= 500) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() + 5);
+                }
+
+            }
+            else if (e.getCode() == KeyCode.LEFT){
+                if(p1.getX() <= 0) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() - 5);
+                }
+            }
+            else if (e.getCode() == KeyCode.DOWN){
+                if(p1.getY()  + p1.getHeight() >= 500) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() + 5);
+                }
+            }
+            else if (e.getCode() == KeyCode.UP){
+                if(p1.getY() <= 0) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() - 5);
+                }
+            }
+        });
+        pane.getChildren().add(p1);
+
+        Scene scene = new Scene(pane, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DODGE THE LASERS");
+        primaryStage.show();
+        pane.requestFocus();
+    }
+    public boolean isIntersect(Bounds p1Bounds, Bounds lineBounds) {
+        if (lineBounds.intersects(p1Bounds)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+//class lineline extends Pane{
+//    public lineline() {
+//        Line line = new Line();
+//        line.setStrokeDashOffset(5);
+//        int direction = (int)(Math.random() * 2);
+//        if(direction == 0){
+//            line.setStartX(0);
+//            line.setStartY(Math.random() * 500);
+//            line.setEndX(500);
+//            line.setEndY(Math.random() * 500);
+//        }
+//        else{
+//            line.setStartX(Math.random() * 500);
+//            line.setStartY(0);
+//            line.setEndX(Math.random() * 500);
+//            line.setEndY(500);
+//        }
+//    }
+//*/
