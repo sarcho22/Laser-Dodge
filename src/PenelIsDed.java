cf91f2252d324a67d9b664373876f56c079df727
penelopeom <penelopeom@aol.com>
2019-06-03 12:39:04 -0400

    yeet yeet i'm not a shrimp

--- src/PenelIsDed.java
+++ src/PenelIsDed.java
@@ -1,73 +1,155 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class PenelIsDed extends Application {
     public void start (Stage primaryStage) {
         Pane pane = new Pane();
 
-//        Rectangle ground1 = new Rectangle();
-//        ground1.setFill(Color.POWDERBLUE);
-//        ground1.setStroke(Color.PALEGOLDENROD);
-//        ground1.setStrokeWidth(1);
-//        ground1.widthProperty().bind(pane.widthProperty().divide(3));
-//        ground1.heightProperty().bind(pane.heightProperty().divide(10));
-//        ground1.setX(0);
-//        ground1.setY(720);
-//        ground1.yProperty().bind(pane.heightProperty().subtract(ground1.heightProperty()));
-//        pane.getChildren().add(ground1);
+        Rectangle p1 = new Rectangle();
+        p1.setFill(Color.POWDERBLUE);
+        p1.setWidth(20);
+        p1.setHeight(20);
+        Bounds p1Bounds = p1.getBoundsInParent();
+
+        int score = 0;
+        boolean p1alive = true;
+
+        while(p1alive) {
+//            lineline laser = new lineline();
+            for(int i = 0; i < score; i++) {
+
+
+                Line line = new Line();
+                line.setFill(Color.WHITE);
+                line.setStroke(Color.RED);
+                pane.getChildren().add(line);
+                Bounds lineBounds = line.getBoundsInParent();
+                int direction = (int)(Math.random() * 2);
+                if(direction == 0){
+                    line.setStartX(0);
+                    line.setStartY(Math.random() * 500);
+                    line.setEndX(500);
+                    line.setEndY(Math.random() * 500);
+                }
+                else{
+                    line.setStartX(Math.random() * 500);
+                    line.setStartY(0);
+                    line.setEndX(Math.random() * 500);
+                    line.setEndY(500);
+                }
+                Timeline timer = new Timeline(
+                        new KeyFrame(Duration.ZERO),
+                        new KeyFrame(Duration.seconds(0.5))
+                );
+//            timer.setAutoReverse(true);
+                timer.setCycleCount(5);
+                if (isIntersect(p1Bounds, lineBounds)) {
+                    p1alive = false;
+                    break;
+                }
+
+
+            }
+            if(p1alive) {
+                score++;
+                pane.getChildren().clear();
+            }
+            else {
+                TextField ded = new TextField("YOU GO AWAY U DIED");
+                break;
+            }
 //
 
-        Circle c = new Circle(15, 15, 20);
-        c.setFill(Color.POWDERBLUE);
-        pane.getChildren().add(c);
 
-        Circle eye1 = new Circle();
-        eye1.centerXProperty().bind(c.centerXProperty().subtract(11));
-        eye1.centerYProperty().bind(c.centerYProperty().subtract(7));
-        eye1.setFill(Color.BLACK);
-        pane.getChildren().add(eye1);
+        }
 
-        Circle eye2 = new Circle();
-        eye2.centerXProperty().bind(c.centerXProperty().add(11));
-        eye2.centerYProperty().bind(c.centerYProperty().subtract(7));
-        eye2.setFill(Color.BLACK);
-        pane.getChildren().add(eye2);
 
-//        Image marioImage = new Image("mario.png");
-//        ImageView mario = new ImageView(marioImage);
-//        mario.setFitHeight(60);
-//        mario.setFitWidth(60);
-//        mario.setX(5);
-//        mario.setY(670);
-//        pane.getChildren().add(mario);
 
         pane.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.RIGHT){
-                c.setCenterX(c.getCenterX() + 5);
+                if(p1.getX() + p1.getWidth() >= 500) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() + 5);
+                }
+
             }
             else if (e.getCode() == KeyCode.LEFT){
-                c.setCenterX(c.getCenterX() - 5);
+                if(p1.getX() <= 0) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() - 5);
+                }
             }
             else if (e.getCode() == KeyCode.DOWN){
-                c.setCenterY(c.getCenterY() + 5);
+                if(p1.getY()  + p1.getHeight() >= 500) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() + 5);
+                }
             }
             else if (e.getCode() == KeyCode.UP){
-                c.setCenterY(c.getCenterY() - 5);
+                if(p1.getY() <= 0) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() - 5);
+                }
             }
         });
+        pane.getChildren().add(p1);
 
-        Scene scene = new Scene(pane, 1000, 800);
+        Scene scene = new Scene(pane, 500, 500);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Mario YEET");
+        primaryStage.setTitle("DODGE THE LASERS");
         primaryStage.show();
         pane.requestFocus();
     }
+    public boolean isIntersect(Bounds p1Bounds, Bounds lineBounds) {
+        if (lineBounds.intersects(p1Bounds)) {
+            return true;
+        }
+        return false;
+    }
 }
+
+//class lineline extends Pane{
+//    public lineline() {
+//        Line line = new Line();
+//        line.setStrokeDashOffset(5);
+//        int direction = (int)(Math.random() * 2);
+//        if(direction == 0){
+//            line.setStartX(0);
+//            line.setStartY(Math.random() * 500);
+//            line.setEndX(500);
+//            line.setEndY(Math.random() * 500);
+//        }
+//        else{
+//            line.setStartX(Math.random() * 500);
+//            line.setStartY(0);
+//            line.setEndX(Math.random() * 500);
+//            line.setEndY(500);
+//        }
+//    }
+//
+//
+//
+//}
+//
+//
