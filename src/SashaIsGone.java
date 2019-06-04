deac8d0e0766e25910770ae2628edb8bfeebe712
alevol22 <alevol22@bergen.org>
2019-06-04 13:56:49 -0400

    my page

--- src/SashaIsGone.java
+++ src/SashaIsGone.java
@@ -1,133 +1,3 @@
-/*import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.geometry.Bounds;
-import javafx.util.Duration;
-
-public class SashaIsGone extends Application {
-    public void start (Stage primaryStage) {
-        Pane pane = new Pane();
-/*
-        Rectangle board = new Rectangle();
-        board.setFill(Color.BEIGE);
-        board.setX(0);
-        board.setY(0);
-        board.widthProperty().bind(pane.widthProperty());
-        board.heightProperty().bind(pane.heightProperty());
-        pane.getChildren().add(board);
-*/
-/*
-        Rectangle p1 = new Rectangle(20, 20, 20, 20);
-        p1.setFill(Color.POWDERBLUE);
-        Bounds p1Bounds = p1.getBoundsInParent();
-        int score = 0;
-        boolean p1alive = true;
-
-        pane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.RIGHT) {
-                if (p1.getX() + p1.getWidth() >= 500) {
-                    p1.setX(p1.getX());
-                } else {
-                    p1.setX(p1.getX() + 5);
-                }
-
-            } else if (e.getCode() == KeyCode.LEFT) {
-                if (p1.getX() <= 0) {
-                    p1.setX(p1.getX());
-                } else {
-                    p1.setX(p1.getX() - 5);
-                }
-            } else if (e.getCode() == KeyCode.DOWN) {
-                if (p1.getY() + p1.getHeight() >= 500) {
-                    p1.setY(p1.getY());
-                } else {
-                    p1.setY(p1.getY() + 5);
-                }
-            } else if (e.getCode() == KeyCode.UP) {
-                if (p1.getY() <= 0) {
-                    p1.setY(p1.getY());
-                } else {
-                    p1.setY(p1.getY() - 5);
-                }
-            }
-        });
-
-        pane.getChildren().add(p1);
-
-        while (p1alive) {
-            for (int i = 0; i < score; i++) {
-
-                Line line = new Line();
-
-                Timeline timeline = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
-                   ae -> {
-                            pane.getChildren().add(line);
-                            line.setStroke(Color.GRAY);
-                   }));
-                        timeline.play();
-
-                Bounds lineBounds = line.getBoundsInParent();
-                int direction = (int) (Math.random() * 2);
-                if (direction == 0) {
-                    line.setStartX(0);
-                    line.setStartY(Math.random() * 500);
-                    line.setEndX(500);
-                    line.setEndY(Math.random() * 500);
-                } else {
-                    line.setStartX(Math.random() * 500);
-                    line.setStartY(0);
-                    line.setEndX(Math.random() * 500);
-                    line.setEndY(500);
-                }
-
-                Timeline timeline1 = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
-                        ae -> line.setStroke(Color.RED)));
-                timeline1.play();
-
-                if (isIntersect(p1Bounds, lineBounds)) {
-                    p1alive = false;
-                    break;
-                }
-            }
-
-            if (p1alive) {
-                score++;
-                pane.getChildren().clear();
-            } else {
-                Label ded = new Label("YOU GO AWAY U DIED");
-                pane.getChildren().add(ded);
-                break;
-            }
-        }
-
-
-        Scene scene = new Scene(pane, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Laser Dodgeball");
-        primaryStage.show();
-        pane.requestFocus();
-    }
-
-    public boolean isIntersect(Bounds p1Bounds, Bounds lineBounds) {
-        if (lineBounds.intersects(p1Bounds)) {
-            return true;
-        }
-        return false;
-    }
-}
-*/
-
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -155,12 +25,15 @@ public class SashaIsGone extends Application {
         p1.setFill(Color.POWDERBLUE);
         p1.setWidth(20);
         p1.setHeight(20);
-        Bounds p1Bounds = p1.getBoundsInParent();
 
         int score = 0;
         boolean p1alive = true;
 
         while (p1alive) {
+
+            // what happens
+            // 1. square + ded (10 sec) 2. grey lines (5 sec) 3. red lines (5-10 sec) 4. clear a level
+
             Line line = new Line();
             line.setStroke(Color.GRAY);
 
@@ -168,7 +41,6 @@ public class SashaIsGone extends Application {
                     Duration.millis(2000),
                     ae -> {}));
 
-            pane.getChildren().add(line);
             int direction = (int) (Math.random() * 2);
             if (direction == 0) {
                 line.setStartX(0);
@@ -183,9 +55,14 @@ public class SashaIsGone extends Application {
                 line.setEndY(500);
             }
 
+            t1.play();
+
             Timeline timeline2 = new Timeline(new KeyFrame(
                     Duration.millis(5000),
-                    ae -> System.out.println("You cleared a level.")));
+                    ae -> {
+                        Label endLevel = new Label("You cleared a level.");
+                        pane.getChildren().add(endLevel);
+                    }));
 
             Timeline timeline1 = new Timeline(new KeyFrame(
                     Duration.millis(5000),
@@ -220,12 +97,9 @@ public class SashaIsGone extends Application {
             score++;
             pane.getChildren().clear();
         }
-        else if(!p1alive) {
+        else{
             Label ded = new Label("YOU GO AWAY U DIED");
             pane.getChildren().add(ded);
-        }
-        else {
-            System.out.println("wot");
         }
 
 
