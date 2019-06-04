a1aa02435ef1be33db6f8a10c2aba47427b18956
alevol22 <alevol22@bergen.org>
2019-06-04 13:21:23 -0400

    my page

--- src/SashaIsGone.java
+++ src/SashaIsGone.java
@@ -1,4 +1,4 @@
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
@@ -13,7 +13,7 @@ import javafx.geometry.Bounds;
 import javafx.util.Duration;
 
 public class SashaIsGone extends Application {
-    public void start (Stage primaryStage) throws InterruptedException {
+    public void start (Stage primaryStage) {
         Pane pane = new Pane();
 /*
         Rectangle board = new Rectangle();
@@ -24,6 +24,7 @@ public class SashaIsGone extends Application {
         board.heightProperty().bind(pane.heightProperty());
         pane.getChildren().add(board);
 */
+/*
         Rectangle p1 = new Rectangle(20, 20, 20, 20);
         p1.setFill(Color.POWDERBLUE);
         Bounds p1Bounds = p1.getBoundsInParent();
@@ -62,8 +63,6 @@ public class SashaIsGone extends Application {
         pane.getChildren().add(p1);
 
         while (p1alive) {
-//          lineline laser = new lineline();
-            //drawLine1 laser = new drawLine1();
             for (int i = 0; i < score; i++) {
 
                 Line line = new Line();
@@ -72,7 +71,7 @@ public class SashaIsGone extends Application {
                         Duration.millis(5000),
                    ae -> {
                             pane.getChildren().add(line);
-                            line.setStroke(Color.WHITE);
+                            line.setStroke(Color.GRAY);
                    }));
                         timeline.play();
 
@@ -89,22 +88,7 @@ public class SashaIsGone extends Application {
                     line.setEndX(Math.random() * 500);
                     line.setEndY(500);
                 }
-//                Timeline timer = new Timeline(
-//                        new KeyFrame(Duration.ZERO),
-//                        new KeyFrame(Duration.seconds(0.5))
-//                );
-////            timer.setAutoReverse(true);
-//                timer.setCycleCount(5);
 
-
-//                try {
-//                    wait(5000);
-//                } catch (Exception e) {}
-//                TimeUnit.SECONDS.wait(2);
-//                if (Thread.interrupted())  {
-//                    throw new InterruptedException();
-//                    // Clears interrupted status!
-//                }
                 Timeline timeline1 = new Timeline(new KeyFrame(
                         Duration.millis(5000),
                         ae -> line.setStroke(Color.RED)));
@@ -141,47 +125,146 @@ public class SashaIsGone extends Application {
         return false;
     }
 }
-
-/*
-class drawLine1 extends Pane{
-    public drawLine1() {
-        Line line = new Line();
-        line.setStrokeDashOffset(5);
-        int direction = (int)(Math.random() * 2);
-        if(direction == 0){
-            line.setStartX(0);
-            line.setStartY(Math.random() * 500);
-            line.setEndX(500);
-            line.setEndY(Math.random() * 500);
-        }
-        else{
-            line.setStartX(Math.random() * 500);
-            line.setStartY(0);
-            line.setEndX(Math.random() * 500);
-            line.setEndY(500);
-        }
-    }
-
-}
 */
 
 
-/*class lineline extends Pane{
-//    public lineline() {
-//        Line line = new Line();
-//        line.setStrokeDashOffset(5);
-//        int direction = (int)(Math.random() * 2);
-//        if(direction == 0){
-//            line.setStartX(0);
-//            line.setStartY(Math.random() * 500);
-//            line.setEndX(500);
-//            line.setEndY(Math.random() * 500);
-//        }
-//        else{
-//            line.setStartX(Math.random() * 500);
-//            line.setStartY(0);
-//            line.setEndX(Math.random() * 500);
-//            line.setEndY(500);
-//        }
-//    }
-//*/
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.concurrent.TimeUnit;
+
+public class SashaIsGone extends Application {
+    public void start (Stage primaryStage) {
+        Pane pane = new Pane();
+
+        Rectangle p1 = new Rectangle();
+        p1.setFill(Color.POWDERBLUE);
+        p1.setWidth(20);
+        p1.setHeight(20);
+        Bounds p1Bounds = p1.getBoundsInParent();
+
+        int score = 0;
+        boolean p1alive = true;
+
+        while (p1alive) {
+            Line line = new Line();
+            line.setStroke(Color.GRAY);
+
+            Timeline t1 = new Timeline(new KeyFrame(
+                    Duration.millis(2000),
+                    ae -> {
+
+                    }));
+
+            pane.getChildren().add(line);
+            Bounds lineBounds = line.getBoundsInParent();
+            int direction = (int) (Math.random() * 2);
+            if (direction == 0) {
+                line.setStartX(0);
+                line.setStartY(Math.random() * 500);
+                line.setEndX(500);
+                line.setEndY(Math.random() * 500);
+            } else {
+                line.setStartX(Math.random() * 500);
+                line.setStartY(0);
+                line.setEndX(Math.random() * 500);
+                line.setEndY(500);
+            }
+
+            //        Timeline timeline = new Timeline(new KeyFrame(
+            //                Duration.millis(2000),
+            //                ae -> {
+            //                }));
+            //        timeline.play();
+            //
+            Timeline yeet = new Timeline(new KeyFrame(
+                    Duration.millis(2000),
+                    ae -> {
+                        line.setStroke(Color.RED);
+                    }));
+            yeet.play();
+
+            if (isIntersect(p1, line)) {
+                p1alive = false;
+
+            }
+        }
+
+        if(p1alive) {
+            score++;
+            pane.getChildren().clear();
+        }
+        else {
+            Label ded = new Label("YOU GO AWAY U DED");
+            pane.getChildren().add(ded);
+
+        }
+//
+
+
+
+
+        pane.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.RIGHT){
+                if(p1.getX() + p1.getWidth() >= 500) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() + 5);
+                }
+
+            }
+            else if (e.getCode() == KeyCode.LEFT){
+                if(p1.getX() <= 0) {
+                    p1.setX(p1.getX());
+                }
+                else {
+                    p1.setX(p1.getX() - 5);
+                }
+            }
+            else if (e.getCode() == KeyCode.DOWN){
+                if(p1.getY()  + p1.getHeight() >= 500) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() + 5);
+                }
+            }
+            else if (e.getCode() == KeyCode.UP){
+                if(p1.getY() <= 0) {
+                    p1.setY(p1.getY());
+                }
+                else {
+                    p1.setY(p1.getY() - 5);
+                }
+            }
+        });
+        pane.getChildren().add(p1);
+
+        Scene scene = new Scene(pane, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DODGE THE LASERS");
+        primaryStage.show();
+        pane.requestFocus();
+    }
+    public boolean isIntersect(Rectangle p1, Line line) {
+        if (p1.getBoundsInParent().intersects(line.getBoundsInParent())) {
+            return true;
+        }
+        return false;
+    }
+}
