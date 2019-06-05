a6b6a3c4c14df0b8af3b0e589a9295531c6240d9
penelopeom <penelopeom@aol.com>
2019-06-04 23:02:34 -0400

    yeet yeet i'm not a shrimp

--- src/LevelOne.java
+++ src/LevelOne.java
@@ -1,16 +1,19 @@
+import com.sun.javafx.geom.ShapePair;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.geometry.Bounds;
 import javafx.util.Duration;
+
+import java.util.Iterator;
 
 public class LevelOne extends Application {
     public void start (Stage primaryStage) {
@@ -18,7 +21,7 @@ public class LevelOne extends Application {
 
         Rectangle p1 = new Rectangle(0, 0, 20, 20);
         p1.setFill(Color.POWDERBLUE);
-        Bounds p1Bounds = p1.getBoundsInParent();
+        Bounds p1Bounds = p1.getBoundsInLocal();
         int score = 0;
         boolean p1alive = true;
 
@@ -54,9 +57,7 @@ public class LevelOne extends Application {
 
         while (p1alive) {
             pane.getChildren().add(p1);
-
             for (int i = 0; i < score; i++) {
-
                 Line line = new Line();
                 line.setStroke(Color.GRAY);
 
@@ -74,54 +75,55 @@ public class LevelOne extends Application {
                 }
 
                 Timeline timeline2 = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
+                        Duration.millis(2000),
                         ae -> System.out.println("You cleared a level.")));
 
                 Timeline timeline1 = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
+                        Duration.millis(2000),
                         ae -> {
                             line.setStroke(Color.RED);
                             timeline2.play();
                         }));
-                Bounds lineBounds = line.getBoundsInParent();
-                if (isIntersect(p1, line)) {
+                Bounds lineBounds = line.getBoundsInLocal();
+//                if ((lineBounds.intersects(p1Bounds))) {
+//                    p1alive = false;
+//                    break;
+//                }
+//                Shape intersect = Shape.intersect(p1, line);
+//                if (intersect.getBoundsInLocal().getWidth() != -1) {
+//                    p1alive = false;
+//                }
+
+                if (intersects(line, p1)) {
                     p1alive = false;
-                    break;
                 }
 
 
-
                 Timeline timeline = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
+                        Duration.millis(2000),
                         ae -> {
                             pane.getChildren().add(line);
                             timeline1.play();
                         }));
 
-
-
-
-
-
                 Timeline start = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
+                        Duration.millis(2000),
                         ae -> {
                             timeline.play();
                         }));
                 start.play();
+
+                if (intersects(line, p1)) {
+                    p1alive = false;
+                }
             }
-
-
-
 
             if (p1alive) {
                 score++;
                 pane.getChildren().clear();
-                System.out.println("hi");
             }
             else if(!p1alive) {
                 Label ded = new Label("YOU GO AWAY U DIED");
-                System.out.println(p1alive);
                 pane.getChildren().add(ded);
                 break;
             }
@@ -138,54 +140,20 @@ public class LevelOne extends Application {
         pane.requestFocus();
     }
 
-    public boolean isIntersect(Rectangle p1, Line line) {
-        if (p1.getBoundsInParent().intersects(line.getBoundsInParent())) {
-            return true;
+    public boolean intersects(Line line, Rectangle p1) {
+        for (double x = line.getStartX(); x <= line.getEndX(); x+=0.1) {
+            double m = (line.getStartY() - line.getEndY()) / (line.getStartX() - line.getEndX());
+            double b = line.getStartY() / (m * line.getStartX());
+            double y = (m * x) + b;
+            if (p1.contains(x, y)) {
+                System.out.println("hi");
+                return true;
+            }
         }
         return false;
     }
+
+
 }
 
-/*
-class drawLine1 extends Pane{
-    public drawLine1() {
-        Line line = new Line();
-        line.setStrokeDashOffset(5);
-        int direction = (int)(Math.random() * 2);
-        if(direction == 0){
-            line.setStartX(0);
-            line.setStartY(Math.random() * 500);
-            line.setEndX(500);
-            line.setEndY(Math.random() * 500);
-        }
-        else{
-            line.setStartX(Math.random() * 500);
-            line.setStartY(0);
-            line.setEndX(Math.random() * 500);
-            line.setEndY(500);
-        }
-    }
 
-}
-*/
-
-
-/*class lineline extends Pane{
-//    public lineline() {
-//        Line line = new Line();
-//        line.setStrokeDashOffset(5);
-//        int direction = (int)(Math.random() * 2);
-//        if(direction == 0){
-//            line.setStartX(0);
-//            line.setStartY(Math.random() * 500);
-//            line.setEndX(500);
-//            line.setEndY(Math.random() * 500);
-//        }
-//        else{
-//            line.setStartX(Math.random() * 500);
-//            line.setStartY(0);
-//            line.setEndX(Math.random() * 500);
-//            line.setEndY(500);
-//        }
-//    }
-//*/
--- src/PenelIsDed.java
+++ src/PenelIsDed.java
@@ -1,178 +1,167 @@
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import com.sun.javafx.geom.ShapePair;
 import javafx.application.Application;
-import javafx.geometry.Bounds;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Bounds;
 import javafx.util.Duration;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Iterator;
 
 public class PenelIsDed extends Application {
-    public void start (Stage primaryStage)throws InterruptedException {
+    public void start(Stage primaryStage) {
         Pane pane = new Pane();
 
-        Rectangle p1 = new Rectangle();
+        Rectangle p1 = new Rectangle(0, 0, 20, 20);
         p1.setFill(Color.POWDERBLUE);
-        p1.setWidth(20);
-        p1.setHeight(20);
         Bounds p1Bounds = p1.getBoundsInParent();
 
-        int score = 0;
-        boolean p1alive = true;
-
-        while(p1alive) {
-//            lineline laser = new lineline();
-            Timeline timeline = new Timeline(new KeyFrame(
-                    Duration.millis(5000),
-                    ae -> System.out.println("hi")));
-            timeline.play();
-            for(int i = 0; i < score; i++) {
-
-
-                Line line = new Line();
-                line.setStroke(Color.GRAY);
-                pane.getChildren().add(line);
-                Bounds lineBounds = line.getBoundsInParent();
-                int direction1 = (int)(Math.random() * 2);
-                if(direction1 == 0){
-                    line.setStartX(0);
-                    line.setStartY(Math.random() * 500);
-                    line.setEndX(500);
-                    line.setEndY(Math.random() * 500);
-                }
-                else{
-                    line.setStartX(Math.random() * 500);
-                    line.setStartY(0);
-                    line.setEndX(Math.random() * 500);
-                    line.setEndY(500);
-                }
-//                Timeline timer = new Timeline(
-//                        new KeyFrame(Duration.ZERO),
-//                        new KeyFrame(Duration.seconds(0.5))
-//                );
-////            timer.setAutoReverse(true);
-//                timer.setCycleCount(5);
-//                try {
-//                    wait(5000);
-//                } catch (Exception e) {}
-//                TimeUnit.SECONDS.wait(2);
-//                if (Thread.interrupted())  {
-//                    throw new InterruptedException();
-//                    // Clears interrupted status!
-//                }
-                Timeline yeet = new Timeline(new KeyFrame(
-                        Duration.millis(5000),
-                        ae -> {
-                            line.setStroke(Color.RED);
-
-                        }));
-                yeet.play();
-
-                if (isIntersect(p1, line)) {
-                    p1alive = false;
-                    break;
-                }
-
-
-            }
-            if(p1alive) {
-                score++;
-                pane.getChildren().clear();
-            }
-            else {
-                Label ded = new Label("YOU GO AWAY U DED");
-                pane.getChildren().add(ded);
-                break;
-            }
-//
-
-
-        }
-
-
-
         pane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.RIGHT){
-                if(p1.getX() + p1.getWidth() >= 500) {
+            if (e.getCode() == KeyCode.RIGHT) {
+                if (p1.getX() + p1.getWidth() >= 500) {
                     p1.setX(p1.getX());
-                }
-                else {
+                } else {
                     p1.setX(p1.getX() + 5);
                 }
 
-            }
-            else if (e.getCode() == KeyCode.LEFT){
-                if(p1.getX() <= 0) {
+            } else if (e.getCode() == KeyCode.LEFT) {
+                if (p1.getX() <= 0) {
                     p1.setX(p1.getX());
-                }
-                else {
+                } else {
                     p1.setX(p1.getX() - 5);
                 }
-            }
-            else if (e.getCode() == KeyCode.DOWN){
-                if(p1.getY()  + p1.getHeight() >= 500) {
+            } else if (e.getCode() == KeyCode.DOWN) {
+                if (p1.getY() + p1.getHeight() >= 500) {
                     p1.setY(p1.getY());
-                }
-                else {
+                } else {
                     p1.setY(p1.getY() + 5);
                 }
-            }
-            else if (e.getCode() == KeyCode.UP){
-                if(p1.getY() <= 0) {
+            } else if (e.getCode() == KeyCode.UP) {
+                if (p1.getY() <= 0) {
                     p1.setY(p1.getY());
-                }
-                else {
+                } else {
                     p1.setY(p1.getY() - 5);
                 }
             }
         });
-        pane.getChildren().add(p1);
 
-        Scene scene = new Scene(pane, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("DODGE THE LASERS");
-        primaryStage.show();
-        pane.requestFocus();
-    }
-    public boolean isIntersect(Rectangle p1, Line line) {
-        if (p1.getBoundsInParent().intersects(line.getBoundsInParent())) {
-            return true;
+        Rectangle r1 = new Rectangle(0, 0, 0, 0);
+
+        EventHandler<ActionEvent> eventHandler = e -> {
+            int score = 1;
+            boolean p1alive = true;
+
+            while (p1alive) {
+                pane.getChildren().add(p1);
+                for (int i = 0; i < score; i++) {
+                    Line line = new Line();
+                    line.setStroke(Color.GRAY);
+
+                    int direction = (int) (Math.random() * 2);
+                    if (direction == 0) {
+                        line.setStartX(0);
+                        line.setStartY(Math.random() * 500);
+                        line.setEndX(500);
+                        line.setEndY(Math.random() * 500);
+                    } else {
+                        line.setStartX(Math.random() * 500);
+                        line.setStartY(0);
+                        line.setEndX(Math.random() * 500);
+                        line.setEndY(500);
+                    }
+
+                    Timeline timeline2 = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> System.out.println("You cleared a level.")));
+
+                    Timeline yes = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                pane.getChildren().clear();
+                                timeline2.play();
+                            }));
+
+                    Timeline no = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                Label ded = new Label("YOU GO AWAY U DIED");
+                                pane.getChildren().add(ded);
+                                r1.setWidth(10);
+                            }));
+
+                    Timeline check = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                boolean intersects = false;
+                                for (double x = line.getStartX(); x <= line.getEndX(); x += 0.1) {
+                                    double m = (line.getStartY() - line.getEndY()) / (line.getStartX() - line.getEndX());
+                                    double b = line.getStartY() - (m * line.getStartX());
+                                    double y = (m * x) + b;
+                                    if (p1.contains(x, y)) {
+                                        System.out.println("hi");
+                                        intersects = true;
+                                    }
+                                }
+                                if (intersects) {
+                                    yes.play();
+                                } else {
+                                    no.play();
+                                }
+                            }));
+
+                    Timeline timeline1 = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                line.setStroke(Color.RED);
+                                check.play();
+                            }));
+
+                    Timeline timeline = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                pane.getChildren().add(line);
+                                timeline1.play();
+                            }));
+
+                    Timeline start = new Timeline(new KeyFrame(
+                            Duration.millis(2000),
+                            ae -> {
+                                timeline.play();
+                            }));
+
+                    start.play();
+
+                    if (r1.getWidth() == 10) {
+                        p1alive = false;
+                        break;
+                    }
+
+                }
+                score++;
+            }
+            };
+
+            Timeline animation = new Timeline(
+                    new KeyFrame(Duration.millis(500), eventHandler));
+            animation.play();
+
+
+            Scene scene = new Scene(pane, 500, 500);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Laser Dodge");
+            primaryStage.show();
+            pane.requestFocus();
         }
-        return false;
-    }
+
 }
 
-//class lineline extends Pane{
-//    public lineline() {
-//        Line line = new Line();
-//        line.setStrokeDashOffset(5);
-//        int direction = (int)(Math.random() * 2);
-//        if(direction == 0){
-//            line.setStartX(0);
-//            line.setStartY(Math.random() * 500);
-//            line.setEndX(500);
-//            line.setEndY(Math.random() * 500);
-//        }
-//        else{
-//            line.setStartX(Math.random() * 500);
-//            line.setStartY(0);
-//            line.setEndX(Math.random() * 500);
-//            line.setEndY(500);
-//        }
-//    }
-//
-//
-//
-//}
-//
-//
+
