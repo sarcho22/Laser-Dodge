9cd6de9a8b34e463c7fa61bf4c4245d5277f2c62
Sarah Choi <sarcho22@bergen.org>
2019-06-05 13:19:05 -0400

    yeet

--- src/YouGoAwaySarah.java
+++ src/YouGoAwaySarah.java
@@ -52,10 +52,12 @@ public class YouGoAwaySarah extends Application {
         Rectangle r1 = new Rectangle(0, 0, 0, 0);
 
         EventHandler<ActionEvent> eventHandler = e -> {
-            int score = 1;
+            int score = 0;
             boolean p1alive = true;
-
             while (p1alive) {
+                score++;
+                System.out.println(score);
+                //
                 pane.getChildren().add(p1);
                 for (int i = 0; i < score; i++) {
                     Line line = new Line();
@@ -79,14 +81,14 @@ public class YouGoAwaySarah extends Application {
                             ae -> System.out.println("You cleared a level.")));
 
                     Timeline yes = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
+                            Duration.millis(1),
                             ae -> {
                                 pane.getChildren().clear();
                                 timeline2.play();
                             }));
 
                     Timeline no = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
+                            Duration.millis(1),
                             ae -> {
                                 Label ded = new Label("YOU GO AWAY U DIED");
                                 pane.getChildren().add(ded);
@@ -94,7 +96,7 @@ public class YouGoAwaySarah extends Application {
                             }));
 
                     Timeline check = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
+                            Duration.millis(200),
                             ae -> {
                                 boolean intersects = false;
                                 for (double x = line.getStartX(); x <= line.getEndX(); x += 0.1) {
@@ -102,7 +104,6 @@ public class YouGoAwaySarah extends Application {
                                     double b = line.getStartY() - (m * line.getStartX());
                                     double y = (m * x) + b;
                                     if (p1.contains(x, y)) {
-                                        System.out.println("hi");
                                         intersects = true;
                                     }
                                 }
@@ -114,26 +115,20 @@ public class YouGoAwaySarah extends Application {
                             }));
 
                     Timeline timeline1 = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
+                            Duration.millis(2500),
                             ae -> {
                                 line.setStroke(Color.RED);
                                 check.play();
                             }));
 
                     Timeline timeline = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
+                            Duration.millis(4000),
                             ae -> {
                                 pane.getChildren().add(line);
                                 timeline1.play();
                             }));
 
-                    Timeline start = new Timeline(new KeyFrame(
-                            Duration.millis(2000),
-                            ae -> {
-                                timeline.play();
-                            }));
-
-                    start.play();
+                    timeline.play();
 
                     if (r1.getWidth() == 10) {
                         p1alive = false;
@@ -141,7 +136,7 @@ public class YouGoAwaySarah extends Application {
                     }
 
                 }
-                score++;
+
             }
         };
 
