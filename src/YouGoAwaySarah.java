3527f0a804044f4aead6c4ef42f19c03963c7d78
Sarah Choi <sarcho22@bergen.org>
2019-06-12 15:06:45 -0400

    yeet

--- src/YouGoAwaySarah.java
+++ src/YouGoAwaySarah.java
@@ -1,26 +1,163 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.util.Duration;
 
-import java.sql.Time;
+import java.util.ArrayList;
 
-// maybe make a pacman type thing where there are dots and we need to get the dots as a challenge to increase score, also high score
-public class YouGoAwaySarah extends Application {
+public class YouGoAwaySarah extends Application{
+    public ArrayList playerScores;
     public int eatenNumber1 = 0;
     public int eatenNumber2 = 0;
-    public void start(Stage primaryStage) {
+    public boolean p1left = false;
+    public boolean p2right = false;
+    public boolean p1up = false;
+    public boolean p1down = false;
+    public boolean p2left = false;
+    public boolean p2down = false;
+    public boolean p2up = false;
+    public boolean p1right = false;
+    public void start(Stage menuStage){
+        BorderPane borderPane = new BorderPane();
+        Rectangle bg = new Rectangle();
+        bg.widthProperty().bind(borderPane.widthProperty());
+        bg.heightProperty().bind(borderPane.heightProperty());
+        bg.setFill(Color.ALICEBLUE);
+        borderPane.getChildren().add(bg);
+
+        BorderPane bp1 = new BorderPane();
+        borderPane.setTop(bp1);
+
+        HBox hBox = new HBox();
+        bp1.setBottom(hBox);
+        HBox hBox1 = new HBox();
+        bp1.setTop(hBox1);
+        HBox hBox2 = new HBox();
+        borderPane.setBottom(hBox2);
+        HBox hBox3 = new HBox();
+        borderPane.setCenter(hBox3);
+        hBox2.setSpacing(10);
+
+        Label credits = new Label("Made by GoAway, IsGone, and JustDED");
+        credits.setFont(new Font("Cambria", 20));
+        Label gameTitle = new Label("\n\nLaser Dodge");
+        gameTitle.setFont(new Font("Cambria", 50));
+        Button play = new Button("Play");
+        play.setFont(new Font("Cambria", 40));
+        Button help = new Button("Help");
+        help.setFont(new Font("Cambria", 40));
+        Label name = new Label("Enter Name: ");
+        name.setFont(new Font("Cambria", 20));
+        TextField nameEntry = new TextField();
+
+        hBox1.getChildren().add(credits);
+        hBox.getChildren().add(gameTitle);
+        hBox2.getChildren().add(play);
+        hBox2.getChildren().add(help);
+        hBox3.getChildren().add(name);
+        hBox3.getChildren().add(nameEntry);
+        hBox.setAlignment(Pos.CENTER);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox3.setAlignment(Pos.CENTER);
+
+
+        play.setOnAction(e -> {
+            // shove the name into the results page!!!
+            menuStage.close();
+            Stage playStage = new Stage();
+            game(playStage);
+            //playerScores.add(0, nameEntry.getText())
+            //playerScores.add(nameEntry.getText());
+        });
+
+        help.setOnAction(e -> {
+            // put code to run the help page here
+            menuStage.close();
+            Stage helpStage = new Stage();
+            help(helpStage);
+
+        });
+
+
+        Scene scene = new Scene(borderPane, 500, 500);
+        menuStage.setScene(scene);
+        menuStage.setTitle("Welcome to Laser Dodge!");
+        menuStage.show();
+        borderPane.requestFocus();
+    }
+
+    public void help(Stage helpStage){
+        BorderPane borderPane = new BorderPane();
+        Rectangle bg = new Rectangle();
+        bg.widthProperty().bind(borderPane.widthProperty());
+        bg.heightProperty().bind(borderPane.heightProperty());
+        bg.setFill(Color.ALICEBLUE);
+        borderPane.getChildren().add(bg);
+
+        HBox hBox1 = new HBox();
+        borderPane.setTop(hBox1);
+        HBox hBox2 = new HBox();
+        borderPane.setBottom(hBox2);
+        HBox hBox3 = new HBox();
+        borderPane.setCenter(hBox3);
+
+        Label gameTitle = new Label("\n\nInstructions");
+        gameTitle.setFont(new Font("Cambria", 50));
+        Button menu = new Button("Return to Menu");
+        menu.setFont(new Font("Cambria", 20));
+        Text instructions = new Text("");
+        instructions.setFont(new Font("Cambria", 20));
+        instructions.setText(
+                "1. You will be a blue square,\n\tuse the arrow keys to avoid touching the lasers.\n" +
+                        "2. You will have 5 seconds to aviod the\n\t gray lasers until they turn red.\n" +
+                        "3. To score more points try to eat the\n\t purple circles that will appear on the screen.\n" +
+                        "4. Try to stay alive as long as you can\n\t and score the most points! Good Luck!"
+        );
+
+        hBox1.getChildren().add(gameTitle);
+        hBox2.getChildren().add(menu);
+        hBox3.getChildren().add(instructions);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox3.setAlignment(Pos.CENTER);
+
+
+        menu.setOnAction(e -> {
+            helpStage.close();
+            Stage menuStage = new Stage();
+            start(menuStage);
+        });
+
+
+        Scene scene = new Scene(borderPane, 500, 500);
+        helpStage.setScene(scene);
+        helpStage.setTitle("Game Help");
+        helpStage.show();
+        borderPane.requestFocus();
+    }
+
+
+    public void game(Stage playStage) {
         BorderPane borderPane = new BorderPane();
         Pane pane = new Pane();
         borderPane.setCenter(pane);
@@ -42,66 +179,161 @@ public class YouGoAwaySarah extends Application {
         gridPane.add(p2eaten, 3, 0);
         gridPane.setHgap(25);
 
-        Rectangle p1 = new Rectangle(0, 240, 20, 20);
+        Rectangle p1 = new Rectangle(480, 240, 20, 20);
         p1.setFill(Color.POWDERBLUE);
 
-        Rectangle p2 = new Rectangle(480, 240, 20, 20);
+        Rectangle p2 = new Rectangle(0, 240, 20, 20);
         p2.setFill(Color.LIGHTPINK);
 
-        pane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.RIGHT) {
-                if (p1.getX() + p1.getWidth() >= 500) {
-                    p1.setX(p1.getX());
-                } else {
-                    p1.setX(p1.getX() + 5);
-                }
+        Circle eye1 = new Circle();
+        eye1.centerXProperty().bind(p1.xProperty().add(7));
+        eye1.centerYProperty().bind(p1.yProperty().add(7));
+        eye1.setRadius(2);
+        eye1.setStrokeWidth(1);
+        eye1.setFill(Color.BLACK);
 
-            } else if (e.getCode() == KeyCode.LEFT) {
-                if (p1.getX() <= 0) {
-                    p1.setX(p1.getX());
-                } else {
-                    p1.setX(p1.getX() - 5);
+        Circle eye2 = new Circle();
+        eye2.centerXProperty().bind(eye1.centerXProperty().add(8));
+        eye2.centerYProperty().bind(eye1.centerYProperty());
+        eye2.setRadius(2);
+        eye2.setStrokeWidth(1);
+        eye2.setFill(Color.BLACK);
+
+        Circle eye3 = new Circle();
+        eye3.centerXProperty().bind(p2.xProperty().add(7));
+        eye3.centerYProperty().bind(p2.yProperty().add(7));
+        eye3.setRadius(2);
+        eye3.setStrokeWidth(1);
+        eye3.setFill(Color.BLACK);
+
+        Circle eye4 = new Circle();
+        eye4.centerXProperty().bind(eye3.centerXProperty().add(8));
+        eye4.centerYProperty().bind(eye3.centerYProperty());
+        eye4.setRadius(2);
+        eye4.setStrokeWidth(1);
+        eye4.setFill(Color.BLACK);
+
+        Line mouth1 = new Line();
+        mouth1.startXProperty().bind(eye1.centerXProperty().add(2));
+        mouth1.startYProperty().bind(eye1.centerYProperty().add(7));
+        mouth1.endXProperty().bind(mouth1.startXProperty().add(5));
+        mouth1.endYProperty().bind(mouth1.startYProperty());
+        mouth1.setStrokeWidth(2);
+        mouth1.setStroke(Color.BLACK);
+
+        Line mouth2 = new Line();
+        mouth2.startXProperty().bind(eye3.centerXProperty().add(2));
+        mouth2.startYProperty().bind(eye3.centerYProperty().add(7));
+        mouth2.endXProperty().bind(mouth2.startXProperty().add(5));
+        mouth2.endYProperty().bind(mouth2.startYProperty());
+        mouth2.setStrokeWidth(2);
+        mouth2.setStroke(Color.BLACK);
+
+        EventHandler<ActionEvent> eventHandler2 = f -> {
+            pane.setOnKeyPressed(e -> {
+                if (e.getCode() == KeyCode.RIGHT) {
+                    p1right = true;
+                } else if (e.getCode() == KeyCode.LEFT) {
+                    p1left = true;
+                } else if (e.getCode() == KeyCode.DOWN) {
+                    p1down = true;
+                } else if (e.getCode() == KeyCode.UP) {
+                    p1up = true;
                 }
-            } else if (e.getCode() == KeyCode.DOWN) {
-                if (p1.getY() + p1.getHeight() >=480) {
-                    p1.setY(p1.getY());
-                } else {
-                    p1.setY(p1.getY() + 5);
+                if (e.getText().equals("d")) {
+                    p2left = true;
+                } else if (e.getText().equals("a")) {
+                    p2right = true;
+                } else if (e.getText().equals("s")) {
+                    p2down = true;
+                } else if (e.getText().equals("w")) {
+                    p2up = true;
                 }
-            } else if (e.getCode() == KeyCode.UP) {
+            });
+
+            pane.setOnKeyReleased(e -> {
+                if (e.getCode() == KeyCode.RIGHT) {
+                    p1right = false;
+                } else if (e.getCode() == KeyCode.LEFT) {
+                    p1left = false;
+                } else if (e.getCode() == KeyCode.DOWN) {
+                    p1down = false;
+                } else if (e.getCode() == KeyCode.UP) {
+                    p1up = false;
+                }
+                if (e.getText().equals("d")) {
+                    p2left = false;
+                } else if (e.getText().equals("a")) {
+                    p2right = false;
+                } else if (e.getText().equals("s")) {
+                    p2down = false;
+                } else if (e.getText().equals("w")) {
+                    p2up = false;
+                }
+            });
+
+            if (p1up) {
                 if (p1.getY() <= 0) {
                     p1.setY(p1.getY());
                 } else {
                     p1.setY(p1.getY() - 5);
                 }
             }
-            if (e.getText().equals("d")) {
-                if (p2.getX() + p2.getWidth() >= 500) {
-                    p2.setX(p2.getX());
-                } else {
-                    p2.setX(p2.getX() + 5);
-                }
-
-            } else if (e.getText().equals("a")) {
-                if (p2.getX() <= 0) {
-                    p2.setX(p2.getX());
-                } else {
-                    p2.setX(p2.getX() - 5);
-                }
-            } else if (e.getText().equals("s")) {
-                if (p2.getY() + p2.getHeight() >= 500) {
-                    p2.setY(p2.getY());
-                } else {
-                    p2.setY(p2.getY() + 5);
-                }
-            } else if (e.getText().equals("w")) {
+            if (p2up) {
                 if (p2.getY() <= 0) {
                     p2.setY(p2.getY());
                 } else {
                     p2.setY(p2.getY() - 5);
                 }
             }
-        });
+            if (p2down) {
+                if (p2.getY() + p2.getHeight() >= 480) {
+                    p2.setY(p2.getY());
+                } else {
+                    p2.setY(p2.getY() + 5);
+                }
+            }
+            if (p1down) {
+                if (p1.getY() + p1.getHeight() >= 480) {
+                    p1.setY(p1.getY());
+                } else {
+                    p1.setY(p1.getY() + 5);
+                }
+            }
+            if (p2left) {
+                if (p2.getX() + p2.getWidth() >= 500) {
+                    p2.setX(p2.getX());
+                } else {
+                    p2.setX(p2.getX() + 5);
+                }
+            }
+            if (p1left) {
+                if (p1.getX() <= 0) {
+                    p1.setX(p1.getX());
+                } else {
+                    p1.setX(p1.getX() - 5);
+                }
+            }
+            if (p2right) {
+                if (p2.getX() <= 0) {
+                    p2.setX(p2.getX());
+                } else {
+                    p2.setX(p2.getX() - 5);
+                }
+            }
+            if (p1right) {
+                if (p1.getX() + p1.getWidth() >= 500) {
+                    p1.setX(p1.getX());
+                } else {
+                    p1.setX(p1.getX() + 5);
+                }
+            }
+        };
+
+        Timeline keys = new Timeline(
+                new KeyFrame(Duration.millis(50), eventHandler2));
+        keys.setCycleCount(Timeline.INDEFINITE);
+        keys.play();
 
         Rectangle r1 = new Rectangle(0, 0, 0, 0);
 
@@ -111,7 +343,7 @@ public class YouGoAwaySarah extends Application {
             boolean p2alive = true;
             r1.setHeight(r1.getHeight() + 1);
 
-            Circle eat = new Circle((Math.random()*480)+10, Math.random()*490,10);
+            Circle eat = new Circle((Math.random()*470)+10, Math.random()*470,10);
             eat.setFill(Color.MEDIUMPURPLE);
             eat.setStroke(Color.LIGHTBLUE);
             pane.getChildren().add(eat);
@@ -121,6 +353,12 @@ public class YouGoAwaySarah extends Application {
                 //
                 pane.getChildren().add(p1);
                 pane.getChildren().add(p2);
+                pane.getChildren().add(eye1);
+                pane.getChildren().add(eye2);
+                pane.getChildren().add(eye3);
+                pane.getChildren().add(eye4);
+                pane.getChildren().add(mouth1);
+                pane.getChildren().add(mouth2);
                 for (int i = 0; i < r1.getHeight(); i++) {
                     Line line = new Line();
                     line.setStroke(Color.GRAY);
@@ -218,7 +456,7 @@ public class YouGoAwaySarah extends Application {
 //                                }
                                 if(boolEaten1) {
                                     eatenNumber1++;
-                                    p1eaten.setText("Player 1: " + eatenNumber1);
+                                    p2eaten.setText("Player 2: " + eatenNumber1);
                                     eat.setRadius(0);
                                     eat.setCenterX(1000);
                                     eat.setCenterY(1000);
@@ -226,7 +464,7 @@ public class YouGoAwaySarah extends Application {
                                 }
                                 if(boolEaten2) {
                                     eatenNumber2++;
-                                    p2eaten.setText("Player 2: " + eatenNumber2);
+                                    p1eaten.setText("Player 1: " + eatenNumber2);
                                     eat.setRadius(0);
                                     eat.setCenterX(1000);
                                     eat.setCenterY(1000);
@@ -244,8 +482,8 @@ public class YouGoAwaySarah extends Application {
                             Duration.millis(500),
                             ae -> {
                                 pane.getChildren().clear();
-                                p1eaten.setText("Player 1: 0");
                                 p2eaten.setText("Player 2: 0");
+                                p1eaten.setText("Player 1: 0");
                                 eatenNumber1 = 0;
                                 eatenNumber2 = 0;
                                 status.setText("Status pending...");
@@ -331,9 +569,75 @@ public class YouGoAwaySarah extends Application {
 
 
         Scene scene = new Scene(borderPane, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Laser Dodge");
-        primaryStage.show();
+        playStage.setScene(scene);
+        playStage.setTitle("Laser Dodge");
+        playStage.show();
         pane.requestFocus();
+    }
+
+    public void end(Stage endStage){
+        BorderPane borderPane = new BorderPane();
+        Rectangle bg = new Rectangle();
+        bg.widthProperty().bind(borderPane.widthProperty());
+        bg.heightProperty().bind(borderPane.heightProperty());
+        bg.setFill(Color.ALICEBLUE);
+        borderPane.getChildren().add(bg);
+
+        BorderPane bp1 = new BorderPane();
+        borderPane.setTop(bp1);
+
+        HBox hBox = new HBox();
+        bp1.setBottom(hBox);
+        HBox hBox1 = new HBox();
+        bp1.setTop(hBox1);
+        HBox hBox2 = new HBox();
+        borderPane.setBottom(hBox2);
+        HBox hBox3 = new HBox();
+        borderPane.setCenter(hBox3);
+        hBox2.setSpacing(10);
+
+        Label gameTitle = new Label("\n\nLeader Board");
+        gameTitle.setFont(new Font("Cambria", 50));
+        Button restart = new Button("Restart");
+        restart.setFont(new Font("Cambria", 40));
+        Button exit = new Button("Exit Game");
+        exit.setFont(new Font("Cambria", 40));
+        Label name = new Label("Enter Name: ");
+        name.setFont(new Font("Cambria", 20));
+        TextField nameEntry = new TextField();
+/*
+        for (int i = 0; i < playerScores.size(); i++){
+            Label resultLine = new Label("\n" + playerScores.get(i) + "\t" + "points");
+            resultLine.setFont(new Font("Cambria", 20));
+            hBox.getChildren().add(resultLine);
+        }
+*/
+        hBox1.getChildren().add(gameTitle);
+        hBox2.getChildren().add(restart);
+        hBox2.getChildren().add(exit);
+        hBox3.getChildren().add(name);
+        hBox3.getChildren().add(nameEntry);
+        hBox.setAlignment(Pos.CENTER);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox3.setAlignment(Pos.CENTER);
+
+
+        restart.setOnAction(e -> {
+            endStage.close();
+            Stage playStage = new Stage();
+            game(playStage);
+        });
+
+        exit.setOnAction(e -> {
+            endStage.close();
+        });
+
+
+        Scene scene = new Scene(borderPane, 500, 500);
+        endStage.setScene(scene);
+        endStage.setTitle("Past Results");
+        endStage.show();
+        borderPane.requestFocus();
     }
 }
